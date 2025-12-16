73f2c01c9889858a256c802675b69c93ffdd25e9
6704062611352 <s6704062611352@email.kmutnb.ac.th>
2025-12-16 09:59:12 +0700

    4.1แก้ไขลืมเป็นFloat

--- ws2expr.c
+++ ws2expr.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-	int a = 9,b=2,c=0,d=1;
-	int A,B,C,D,E;
+	float a = 1,b=1,c=5,d=1;
+	float A,B,C,D,E;
 	A = 3*(a/5)+(1/b);
 	B = (3*a+5*b)/(2+c);
 	C = (2/7)*(pow(4,3+c)-5*d);
 	D = sqrt((2+8*b)/a);
 	E = cbrt(pow(b,2)-4*d);
-	printf("%d\n%d\n%d\n%d\n%d",A,B,C,D,E);
+	printf("%.2f\n%.2f\n%.2f\n%.2f\n%.2f",A,B,C,D,E);
+	return 0;
 }
